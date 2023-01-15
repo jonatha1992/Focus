9ea7f99f039f9e419406e2f890c431d5d4b03371
jonatha1992 <jonathancorrea291292@hotmail.com>
2023-01-14 23:10:06 -0300

    Agregar archivos de proyecto.

---
+++ Transcripcion/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Transcripcion
+{
+    partial class Trancripciones
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Trancripciones));
+            this.buttonAgregar = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listaCanciones = new System.Windows.Forms.ListBox();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.Reproductor = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonAgregar
+            // 
+            this.buttonAgregar.BackColor = System.Drawing.Color.Green;
+            this.buttonAgregar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAgregar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonAgregar.Location = new System.Drawing.Point(791, 431);
+            this.buttonAgregar.Name = "buttonAgregar";
+            this.buttonAgregar.Size = new System.Drawing.Size(102, 46);
+            this.buttonAgregar.TabIndex = 2;
+            this.buttonAgregar.Text = "Seleccionar Audio";
+            this.buttonAgregar.UseVisualStyleBackColor = false;
+            this.buttonAgregar.Click += new System.EventHandler(this.buttonAgregar_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label.Location = new System.Drawing.Point(12, 616);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(140, 13);
+            this.label.TabIndex = 3;
+            this.label.Text = "Produccón Correa Jonathan";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(772, 504);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(809, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Lista de Reproducción";
+            // 
+            // listaCanciones
+            // 
+            this.listaCanciones.AllowDrop = true;
+            this.listaCanciones.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.listaCanciones.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listaCanciones.CausesValidation = false;
+            this.listaCanciones.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaCanciones.FormattingEnabled = true;
+            this.listaCanciones.ItemHeight = 16;
+            this.listaCanciones.Location = new System.Drawing.Point(791, 100);
+            this.listaCanciones.Name = "listaCanciones";
+            this.listaCanciones.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listaCanciones.Size = new System.Drawing.Size(214, 306);
+            this.listaCanciones.TabIndex = 7;
+            this.listaCanciones.SelectedIndexChanged += new System.EventHandler(this.listaCanciones_SelectedIndexChanged);
+            this.listaCanciones.DragDrop += new System.Windows.Forms.DragEventHandler(this.listaCanciones_DragDrop);
+            this.listaCanciones.DragEnter += new System.Windows.Forms.DragEventHandler(this.listaCanciones_DragEnter);
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.BackColor = System.Drawing.Color.Red;
+            this.buttonEliminar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEliminar.Location = new System.Drawing.Point(920, 431);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(85, 46);
+            this.buttonEliminar.TabIndex = 8;
+            this.buttonEliminar.Text = "Eliminar Audio";
+            this.buttonEliminar.UseVisualStyleBackColor = false;
+            this.buttonEliminar.Click += new System.EventHandler(this.buttonEliminar_Click);
+            // 
+            // Reproductor
+            // 
+            this.Reproductor.Enabled = true;
+            this.Reproductor.Location = new System.Drawing.Point(3, 12);
+            this.Reproductor.Name = "Reproductor";
+            this.Reproductor.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Reproductor.OcxState")));
+            this.Reproductor.Size = new System.Drawing.Size(772, 81);
+            this.Reproductor.TabIndex = 0;
+            this.Reproductor.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.Reproductor_PlayStateChange);
+            // 
+            // Trancripciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1017, 638);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.listaCanciones);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.buttonAgregar);
+            this.Controls.Add(this.Reproductor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Trancripciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Focus";
+            ((System.ComponentModel.ISupportInitialize)(this.Reproductor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonAgregar;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listaCanciones;
+        private AxWMPLib.AxWindowsMediaPlayer Reproductor;
+        private System.Windows.Forms.Button buttonEliminar;
+    }
+}
+
---
+++ transcripciones_usando_microfono/Transcriptor/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.AudioFormat;
+using System.Globalization;
+
+namespace Transcriptor
+{
+    public partial class Form1 : Form
+    {
+        CultureInfo ci = new CultureInfo("en-US");
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        Grammar dictation = new DictationGrammar();
+        static bool completed = false;
+        static string reconocido;
+        SpeechRecognitionEngine recognizer;
+
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recognizer = new SpeechRecognitionEngine(ci);
+            dictation.Name = "diccionario";
+            recognizer.LoadGrammar(dictation);
+            recognizer.SpeechRecognized +=
+            new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+            recognizer.RecognizeCompleted +=
+              new EventHandler<RecognizeCompletedEventArgs>(recognizer_RecognizeCompleted);
+
+
+
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                textBox1.Text = path;
+            }
+        }
+
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result != null && e.Result.Text != null)
+            {
+
+                reconocido = e.Result.Text;
+            }
+            else
+            {
+                MessageBox.Show("No se pudo reconocer nada");
+            }
+        }
+
+        static void recognizer_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Error en el reconocimiento");
+            }
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Operacion cancelada");
+            }
+            if (e.InputStreamEnded)
+            {
+                MessageBox.Show("Se llego al final del audio");
+            }
+            completed = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recognizer.SetInputToWaveFile(path);
+            recognizer.Recognize();
+            listBox1.Items.Add(reconocido);
+
+        }
+    }
+}
